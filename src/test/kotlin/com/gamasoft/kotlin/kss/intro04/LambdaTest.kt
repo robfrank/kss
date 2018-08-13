bcfb3fc513fddfa862344e67cdcc135ef946291d
robfrank <ro.franchini@gmail.com>
2018-08-13 14:44:13 +0200

    test passed:  LambdaTest

--- src/test/kotlin/com/gamasoft/kotlin/kss/intro04/LambdaTest.kt
+++ src/test/kotlin/com/gamasoft/kotlin/kss/intro04/LambdaTest.kt
@@ -8,10 +8,10 @@ import java.util.*
 class LambdaTest {
 
     @Test
-    fun simpleLambda(){
+    fun simpleLambda() {
 
         // fix it to make test pass
-        val tens = Array<Int>(10){ x -> x*10}
+        val tens = Array<String>(10) { x -> (x * 10).toString() }
 
 
         assert(tens[3]).isEqualTo("30")
@@ -20,10 +20,10 @@ class LambdaTest {
     }
 
     @Test
-    fun itIsTheParam(){
+    fun itIsTheParam() {
 
         // fix it to make test pass
-        val duplicate: (String) -> String = {it + "a"}
+        val duplicate: (String) -> String = { it + it }
 
         assert(duplicate("a")).isEqualTo("aa")
         assert(duplicate("b")).isEqualTo("bb")
@@ -31,15 +31,15 @@ class LambdaTest {
     }
 
     @Test
-    fun apply(){
+    fun apply() {
 
         // fix it to make test pass
         fun addProps(properties: Properties?) =
-            properties?.apply {
-                put("name", "John")
-                put("age", 32)
-                put("city", "London")
-            }
+                properties?.apply {
+                    put("name", "Frank")
+                    put("age", 32)
+                    put("city", "London")
+                }
 
         val pr = addProps(Properties())
         val pn = addProps(null)
@@ -51,12 +51,12 @@ class LambdaTest {
     }
 
     @Test
-    fun lambdaType(){
+    fun lambdaType() {
 
         val myLambda: (Int) -> String
 
         //replace todo with an actual lambda and pass the test
-        myLambda = TODO()
+        myLambda = { x -> "value of x is $x" }
 
         assert(myLambda(4)).isEqualTo("value of x is 4")
 
@@ -64,39 +64,39 @@ class LambdaTest {
 
 
     @Test
-    fun functionAsParam(){
+    fun functionAsParam() {
         //fix the code to pass the test
-        fun applyFun(x: String, f: (String) -> String): String = TODO()
+        fun applyFun(x: String, f: (String) -> String): String = f(x)
 
-        assert (applyFun("Abc"){it + it}).isEqualTo("AbcAbc")
-        assert (applyFun("Zack"){it.toLowerCase()}).isEqualTo("zack")
+
+        assert(applyFun("Abc") { it + it }).isEqualTo("AbcAbc")
+        assert(applyFun("Zack") { it.toLowerCase() }).isEqualTo("zack")
 
     }
 
 
     @Test
-    fun functionAsReturnValue(){
+    fun functionAsReturnValue() {
 
         //fix the code to pass the test
-        fun plusFactory(x: Int): (Int) -> Int = TODO()
+        fun plusFactory(x: Int): (Int) -> Int = { y -> x + y }
 
         val plus3 = plusFactory(3)
-        assert (plus3(4)).isEqualTo(7)
+        assert(plus3(4)).isEqualTo(7)
 
     }
 
 
-
     @Test
-    fun higherOrderFunctions(){
+    fun higherOrderFunctions() {
 
         val combine: ((Int) -> Int, (Int) -> Int) -> ((Int) -> Int)
 
-        val square: (Int) -> Int = {it * it}
-        val double: (Int) -> Int = {x -> x * 2 + 1}
+        val square: (Int) -> Int = { it * it }
+        val double: (Int) -> Int = { x -> x * 2 + 1 }
 
         //replace todo with an actual lambda and pass the test
-        combine = TODO()
+        combine = { f, g -> { it -> g(f(it)) } }
         val doubleSquarePlusOne = combine(square, double)
 
         assert(doubleSquarePlusOne(5)).isEqualTo(51)
